fe6f73824f4dd6e1d29a4c50ad1345331a876122
darrylnnon <darrylnnon@gmil.com>
2023-11-16 18:31:36 +0200

    task

--- add.c
+++ add.c
@@ -1,31 +1,29 @@
 #include "monty.h"
 /**
- * monty_add - add  the first two elements of stack.
- * @head: pointer to the head of the stack
+ * montyAdd - func sums the first two elements of stack.
+ * @head: param head
  * @counter: line counter
 */
-void monty_add(stack_t **head, unsigned int counter)
+void montyAdd(stack_t **head, unsigned int counter)
 {
 	int length;
-	stack_t *current_node;
-	int result;
-	current_node = *head;
+	stack_t *h;
+	int my_result;
 
-	for (stack_length = 0; current_node != NULL; stack_length++)
-		current_node = current_node->next;
-	/* check if the stack has at least two elements*/
-
-	if (stack_length < 2)
+	h = *head;
+	for (length = 0; h != NULL; length++)
+		h = h->next;
+	if (length < 2)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", counter);
 		fclose(stub.p_file);
 		free(stub.cont);
-		free_stack(*head);
+		clear_me(*head);
 		exit(EXIT_FAILURE);
 	}
-	current_node= *head;
-	result = current_node->n + current_node->next->n;
-	current_node->next->n = result;
-	*head = current_node->next;
-	free(current_node);
+	h = *head;
+	my_result = h->n + h->next->n;
+	h->next->n = my_result;
+	*head = h->next;
+	free(h);
 }
